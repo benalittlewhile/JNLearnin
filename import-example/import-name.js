d22f4e7810c890fc19df9de9b8efe033ad8dac91
Dewrong <ysidda33@gmail.com>
2022-06-07 15:05:13 -0500

    now I fixed it?

--- import-example/import-name.js
+++ import-example/import-name.js
@@ -1,8 +1,3 @@
 import {problemChild, finder} from "./modules/export-name.js";
 
-
-<<<<<<< Updated upstream
-console.log(`${problemChild} Uncle ${finder}, you're the best at finding people!`);
-=======
 console.log(`${problemChild} ${finder}, you're the best at finding people!`);
->>>>>>> Stashed changes
